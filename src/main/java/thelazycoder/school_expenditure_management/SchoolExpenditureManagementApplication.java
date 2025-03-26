2a34bc62fa634240bf0757042fef64c987cc1ba8
DevTom <72770851+Olatomiw@users.noreply.github.com>
2025-03-26 15:09:13 +0100

    Setup With dependencies

---
+++ src/main/java/thelazycoder/school_expenditure_management/SchoolExpenditureManagementApplication.java
@@ -0,0 +1,13 @@
+package thelazycoder.school_expenditure_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolExpenditureManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolExpenditureManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thelazycoder/school_expenditure_management/SchoolExpenditureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package thelazycoder.school_expenditure_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolExpenditureManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
